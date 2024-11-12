78980c57fdd95d70942c5457428257b1b071e064
Singh <suraj.b.singh@capgemini.com>
2024-11-12 15:39:47 +0530

    user card and button and table correcttion is added

---
+++ src/components/Card/UserCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const UserCard = (prop) => {
+  const { accountDetails } = prop;
+  return (
+    <div className="p-10">
+      <h2 className="text-lg font-bold mb-4">Account Information</h2>
+      {accountDetails.map((detail, index) => (
+        <div key={index} className="mb-2 ml-2">
+          <div className="grid items-stretch grid-cols-2">
+            <div className="text-left font-bold m-0 leading-none">
+              {detail.label}
+            </div>
+            <div className="text-left">
+              <p className="text-xs m-0 leading-none">{detail.value}</p>
+              <Link
+                to={detail.link}
+                className="text-blue-500 text-xs hover:underline m-0 leading-none block"
+              >
+                {detail.linkText}
+              </Link>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default UserCard;
